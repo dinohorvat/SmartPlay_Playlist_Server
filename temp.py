4c3a179a432e6f687b6b8d057428ecb02d8dfd91
Dino Horvat <dxh3401@rit.edu>
2019-01-11 14:09:22 +0100

    Thread play stop rerun

--- temp.py
+++ temp.py
@@ -1,13 +1,12 @@
 from flask import Flask, request
-import time
 import threading
 
 app = Flask(__name__)
 
 playing = 0
-current_index = 0
 duration = 5
 media_files = []
+condition = threading.Condition()
 
 
 def play_media():
@@ -16,22 +15,39 @@ def play_media():
     print(duration)
     while playing == 1:
         for file in media_files:
+            if playing == 0:
+                break
             print(file['path'])
-            time.sleep(duration)
+            with condition:
+                condition.wait(timeout=duration)
     print('Finished')
-
-
-thread1 = threading.Thread(name='playMedia', target=play_media)
 
 
 @app.route('/play', methods=['POST'])
 def play():
-    global duration, media_files
+    global duration, media_files, playing
     media = request.json
     duration = media['duration']
     media_files = media['mediaFiles']
-    thread1.start()
+    threading.Thread(name='playMedia', target=play_media).start()
     return 'Played'
+
+
+@app.route('/stop')
+def stop_media():
+    global playing
+    playing = 0
+    with condition:
+        condition.notify()
+    return 'Stopped'
+
+
+@app.route('/test')
+def test():
+    global playing
+    playing = 1
+    threading.Thread(name='playMedia', target=play_media).start()
+    return 'test'
 
 
 if __name__ == '__main__':
